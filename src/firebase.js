c3a4dc73809eb8d04327a6072ee1194176883a67
shubham tyagi <tyagi.shubham177@gmail.com>
2024-09-24 17:12:15 +0530

    Protect firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Replace the following config object with your Firebase project's config
 const firebaseConfig = {
-  apiKey: "AIzaSyCtJhnjGozdnm2fU5eQQSh8WN8ipVX-P3E",
-  authDomain: "fading-tictactoe.firebaseapp.com",
-  projectId: "fading-tictactoe",
-  storageBucket: "fading-tictactoe.appspot.com",
-  messagingSenderId: "211944890149",
-  appId: "1:211944890149:web:731db4ad6b552e6ccc84cc",
-  measurementId: "G-TKDZ67C09G",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
 
 export { db };
